21885bdf4a8d92256a53399f3d89b8a01f325689
ChandrashekarBemagoni <chandrashekar.bemagoni@gmail.com>
2022-02-28 15:21:48 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req,res) => {
   res.json({
-    msg:'Hello backend'
+    msg:'Hello backendb  test from githubactions '
   })
 })
 app.listen(3001, () => console.log(`Example app listening at http://localhost:${3001}`))
